e18bb91aff9c14249df1153a8fe873969c9c321e
Rugved Agasti <rugvedagasti7@gmail.com>
2022-08-16 14:57:19 +0530

    Add files via upload

---
+++ demo10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a positive integer";
+    cin>>n;
+
+    //for (int i = 1 ; i<=100 ; i++)
+//{
+//cout<<i<<endl;
+
+//}
+//while loop
+
+//int i = 1;
+//while(i<=50){
+  //  cout<<i<<endl;
+    //i++;
+//}
+
+
+
+//exmple
+for (int i = 1 ; i<=10 ; i++)
+{
+    cout<<n*i<<endl;
+} 
+    return 0;
+}
+
+
+
+
